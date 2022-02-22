c8ef3fbb5c1d14caa201ab23b2522f33b765d7fa
Dominik Míček <dominik.micek@notum.cz>
2022-02-22 09:50:31 +0100

    env api

--- code/constants.ts
+++ code/constants.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-export const baseApiUrl =  'http://localhost:3000/api';
+export const baseApiUrl = process.env.NEXT_PUBLIC_API || 'http://localhost:3000/api';
 
 export const axiosInstance = axios.create({ baseURL: baseApiUrl });
