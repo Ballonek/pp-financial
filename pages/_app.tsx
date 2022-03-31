fef02928f8117a674043912d785b987e333eb51f
Dominik Míček <ondrej.janosik@notum.cz>
2022-03-31 10:23:35 +0200

    ignore and title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -130,7 +130,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={client}>
       <Head>
-        <title>PP Financial</title>
+        <title>Investhy</title>
         <meta name='description' />
         <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no' />
         <link rel='icon' href='/favicon.ico' />
